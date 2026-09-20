6c5bda0329717d9de64ee3799085abb476dfbfe7
hammadshafiqd <hammadshafiq2005@gmail.com>
2026-09-20 13:16:23 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+// Firebase web app settings for the "Wapsi App" project.
+// These values are public by design; your data is protected by firestore.rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFJ-DmaR6D2Gnn-nYzd_lm09SL7zDjmdU",
+  authDomain: "wapsi-app.firebaseapp.com",
+  projectId: "wapsi-app",
+  appId: "1:880015103192:web:d8d3a7f7b61ef1f546b3ad"
+};
